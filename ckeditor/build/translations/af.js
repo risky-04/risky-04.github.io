61ace9c548d9f46c3201d28bdec7e9de2ad33c21
risky-04 <riskyirfan03@gmail.com>
2022-11-11 10:31:12 +0700

    initial commit

---
+++ ckeditor/build/translations/af.js
@@ -0,0 +1,1 @@
+(function(n){const e=n["af"]=n["af"]||{};e.dictionary=Object.assign(e.dictionary||{},{"%0 of %1":"%0 van %1","Block quote":"Verwysingsaanhaling",Cancel:"Kanselleer",Save:"Stoor","Show more items":"Wys meer items"});e.getPluralForm=function(n){return n!=1}})(window.CKEDITOR_TRANSLATIONS||(window.CKEDITOR_TRANSLATIONS={}));
